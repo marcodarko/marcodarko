05831de8269400f620fc7c472f120b110ea34230
marco alvarado <Marco@marcos-MacBook-Pro.local>
2017-04-19 16:05:39 -0700

    contact page illustrations

--- JS/contact.js
+++ JS/contact.js
@@ -3,11 +3,13 @@
 $('#call').hover(function () {
 
 		$('#contactBox').toggleClass('left');
+		$('#call').toggleClass('jello-horizontal');
 
 });
 
 $('#email').hover(function () {
 
 		$('#contactBox').toggleClass('right');
+		$('#email').toggleClass('jello-horizontal');
 
 });
