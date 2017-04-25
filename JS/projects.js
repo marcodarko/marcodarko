21efb1ca3b31e5736fb4bd888adeadd4a238e219
marco alvarado <Marco@marcos-MacBook-Pro.local>
2017-04-24 17:23:10 -0700

    changed projects

--- JS/projects.js
+++ JS/projects.js
@@ -1,11 +1,21 @@
 $("#Grindrmodal").iziModal();
 
 $(document).on('click', '.Grindrtrigger', function (event) {
-
     event.preventDefault();
-
     $('#Grindrmodal').iziModal('open');
+});
 
+$('#gaymoji').mouseenter(function(){
+
+	var button1= $('<buttom>').addClass('themeButton roll-in-left ').html("Illustrator");
+	var button2= $('<buttom>').addClass('themeButton roll-in-left ').html("Photoshop");
+	var button3= $('<buttom>').addClass('themeButton roll-in-left ').html("Adobe Draw");
+	var div= $('<div>').append(button1).append(button2).append(button3);
+	$('#gaymoji').append(div);
+});
+
+$('#gaymoji').mouseleave(function(){
+	$('#gaymoji').html(" ");
 });
 
 
@@ -13,9 +23,24 @@ $(document).on('click', '.Grindrtrigger', function (event) {
 $("#ARMSmodal").iziModal();
 
 $(document).on('click', '.ARMStrigger', function (event) {
-
     event.preventDefault();
-
     $('#ARMSmodal').iziModal('open');
-
 });
+
+$('#ARMSurfing').mouseenter(function(){
+
+	var button1= $('<buttom>').addClass('themeButton roll-in-left ').html("HTML 5");
+	var button2= $('<buttom>').addClass('themeButton roll-in-left ').html("CSS 3");
+	var button3= $('<buttom>').addClass('themeButton roll-in-left ').html("JS");
+	var button4= $('<buttom>').addClass('themeButton roll-in-left ').html("JQuery");
+	var button5= $('<buttom>').addClass('themeButton roll-in-left ').html("Web API");
+	var button6= $('<buttom>').addClass('themeButton roll-in-left ').html("Firebase");
+
+	var div= $('<div>').append(button1).append(button2).append(button3).append(button4).append(button5).append(button6);
+	$('#ARMSurfing').append(div);
+});
+
+$('#ARMSurfing').mouseleave(function(){
+	$('#ARMSurfing').html(" ");
+});
+
