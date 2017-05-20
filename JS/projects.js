8b413f9f91e0dc9c84bacebe84ef245303759feb
marco <artofmarco@gmail.com>
2017-05-20 09:32:44 -0700

    adding meta tags

--- JS/projects.js
+++ JS/projects.js
@@ -20,7 +20,6 @@ $('#gaymoji').mouseleave(function(){
 });
 
 
-
 $("#ARMSmodal").iziModal();
 
 $(document).on('click', '.ARMStrigger', function (event) {
@@ -72,6 +71,37 @@ $('#burger').mouseleave(function(){
 	$('#burger').html(" ");
 });
 
+
+
+$("#gitmemodal").iziModal();
+
+$(document).on('click', '.gitmetrigger', function (event) {
+    event.preventDefault();
+    $('#gitmemodal').iziModal('open');
+});
+
+$('#gitme').mouseenter(function(){
+
+	var h2= $('<h2 style="color: white">').html('GitMe');
+	var button1= $('<buttom>').addClass('themeButton roll-in-left ').html("HTML5");
+	var button2= $('<buttom>').addClass('themeButton roll-in-left ').html("CSS3");
+	var button3= $('<buttom>').addClass('themeButton roll-in-left ').html("Passport");
+	var button4= $('<buttom>').addClass('themeButton roll-in-left ').html("JQuery");
+	var button5= $('<buttom>').addClass('themeButton roll-in-left ').html("Node");
+	var button6= $('<buttom>').addClass('themeButton roll-in-left ').html("MySQL");
+	var button7= $('<buttom>').addClass('themeButton roll-in-left ').html("Mocha");
+	var button8= $('<buttom>').addClass('themeButton roll-in-left ').html("D3");
+	var button9= $('<buttom>').addClass('themeButton roll-in-left ').html("AWS");
+	var button10= $('<buttom>').addClass('themeButton roll-in-left ').html("Handlebars");
+
+
+	var div= $('<div>').append(h2).append(button3).append(button4).append(button5).append(button6).append(button7).append(button8).append(button9).append(button10);
+	$('#gitme').append(div);
+});
+
+$('#gitme').mouseleave(function(){
+	$('#gitme').html(" ");
+});
 
 
 
